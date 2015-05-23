c652cf8a8f8209b34582a65bdc2cf9ee7d8893f3
VonC <vonc@laposte.net>
2015-05-24 01:30:43 +0200

    Uses goseapi.Verbose. Get question OP

--- ascom/ascom.go
+++ ascom/ascom.go
@@ -12,8 +12,24 @@ var re = regexp.MustCompile(`<a href="([^"]+)".*?>(.*?)</a>`)
 
 func main() {
 	questionid := os.Args[1]
+	goseapi.Verbose = true
+
+	var questions []goseapi.Question
+	_, err := goseapi.Do("/questions/"+questionid, &questions, &goseapi.Params{
+		Site:  goseapi.StackOverflow,
+		Sort:  goseapi.SortScore,
+		Order: "desc",
+	})
+	if err != nil {
+		fmt.Printf("Unable to fetch question for %s: '%v'", questionid, err)
+		os.Exit(1)
+	}
+	question := questions[0]
+	op := question.Owner
+	fmt.Printf("The [OP %s](%s)\n\n\n", op.DisplayName, op.Link)
+
 	var answers []goseapi.Answer
-	_, err := goseapi.Do("/questions/"+questionid+"/answers", &answers, &goseapi.Params{
+	_, err = goseapi.Do("/questions/"+questionid+"/answers", &answers, &goseapi.Params{
 		Site:  goseapi.StackOverflow,
 		Sort:  goseapi.SortScore,
 		Order: "desc",
@@ -23,7 +39,7 @@ func main() {
 		os.Exit(1)
 	}
 	for _, answer := range answers {
-		fmt.Println(answer.ID)
+		fmt.Printf("--------------\nAnswer '%d':\n\n", answer.ID)
 		var comments []goseapi.Comment
 		_, err := goseapi.Do(fmt.Sprintf("/answers/%d/comments", answer.ID), &comments, &goseapi.Params{
 			Site:   goseapi.StackOverflow,
